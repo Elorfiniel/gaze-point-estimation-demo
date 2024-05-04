70c618367c44906f7a224b5c26bd0762f69fa1cb
Elorfiniel <markgenthusiastic@gmail.com>
2024-05-05 01:43:34 +0800

    Add: common operations in the pipeline of gaze point estimation

---
+++ estimator/runtime/pipeline.py
@@ -0,0 +1,80 @@
+import cv2  # OpenCV-Python
+import numpy as np
+import onnx, onnxruntime
+import os.path as osp
+import torch as torch
+
+
+def load_model(config_path, model_path):
+  config_root = osp.dirname(osp.abspath(config_path))
+  model_path = osp.join(config_root, model_path)
+
+  model = onnx.load_model(model_path)
+  onnx.checker.check_model(model)
+  model = onnxruntime.InferenceSession(model_path)
+
+  return model
+
+
+def prepare_input_image_crop(cv2_image):
+  np_image = np.array(cv2_image, dtype=np.float32) / 255.0
+
+  mean = np.array([0.485, 0.456, 0.406])
+  std = np.array([0.229, 0.224, 0.225])
+  normed_image = (np_image - mean) / std
+
+  np_image = np.transpose(normed_image, axes=(2, 0, 1))
+
+  np_image = np_image.astype(dtype=np.float32)
+  np_image = np.expand_dims(np_image, axis=0)
+
+  return np_image
+
+def prepare_input_key_points(face_bbox, eye_ldmks):
+  kpts_ip = np.concatenate([face_bbox, eye_ldmks], axis=0)
+
+  kpts_ip = kpts_ip.astype(dtype=np.float32)
+  kpts_ip = np.expand_dims(kpts_ip, axis=0)
+
+  return kpts_ip
+
+def prepare_model_input(face_crop, reye_crop, leye_crop, eye_ldmks,
+                        face_resize=(224, 224), eyes_resize=(224, 224)):
+  # Get resized crops: ndarray of shape (h, w, c)
+  face = cv2.resize(face_crop.get_crop(), face_resize, interpolation=cv2.INTER_CUBIC)
+  reye = cv2.resize(reye_crop.get_crop(), eyes_resize, interpolation=cv2.INTER_CUBIC)
+  leye = cv2.resize(leye_crop.get_crop(), eyes_resize, interpolation=cv2.INTER_CUBIC)
+
+  # Convert inputs to ndarrays requested by the estimation model
+  model_input = {
+    'face': prepare_input_image_crop(face),
+    'reye': prepare_input_image_crop(reye),
+    'leye': prepare_input_image_crop(leye),
+    'kpts': prepare_input_key_points(face_crop.get_sas(), eye_ldmks),
+  }
+
+  return model_input
+
+
+def rotate_vector(x, y, theta):
+  theta = np.deg2rad(theta)
+  cos, sin = np.cos(theta), np.sin(theta)
+
+  M = np.array([
+    [cos, -sin],
+    [sin, cos]
+  ], dtype=np.float32)
+  v = np.array([x, y], dtype=np.float32)
+
+  return np.transpose(np.dot(M, np.transpose(v)))
+
+def do_model_inference(model, model_input, theta):
+  ort_outputs = model.run(None, model_input)
+  gcx, gcy = ort_outputs[0].squeeze(0).tolist()
+
+  # Gaze point predicted by model should be projected from prediction space
+  # back into camera coordinate space, by rotating around origin with `theta`
+  #   gx, gy = rotate_vector(gcx, gcy, theta).tolist()
+  gaze_vec = rotate_vector(gcx, gcy, theta)
+
+  return gaze_vec
