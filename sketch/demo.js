c07098bdac7a0bc6d2af2e80ba9812f01ed37333
Elorfiniel <markgenthusiastic@gmail.com>
2024-05-05 16:30:22 +0800

    Update: init template for p5 project

--- sketch/demo.js
+++ sketch/demo.js
@@ -1,7 +1,9 @@
 function setup() {
-
+  const canvas = document.getElementById('canvas');
+  createCanvas(innerWidth, innerHeight, P2D, canvas);
 }
 
 function draw() {
-
+  background(color(255, 255, 255));
+  circle(mouseX, mouseY, 20);
 }
