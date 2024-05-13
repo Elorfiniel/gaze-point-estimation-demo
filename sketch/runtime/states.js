08faa877973c7dcbce3a90552909548c341b0e93
Elorfiniel <markgenthusiastic@gmail.com>
2024-05-13 11:37:04 +0800

    Add: game states and manager

---
+++ sketch/runtime/states.js
@@ -0,0 +1,44 @@
+class GameStates {
+  constructor() {
+    this.INTRO = 'INTRO'
+    this.ONCAM = 'ONCAM'
+    this.GAME = 'GAME'
+    this.CLOSE = 'CLOSE'
+    this.OUTRO = 'OUTRO'
+  }
+}
+
+class GameStateManager {
+  constructor() {
+    this.states = new GameStates()
+    this.resetStates()
+  }
+
+  resetStates() {
+    this.present = this.states.INTRO
+    this.future = undefined
+    this.renewed = true
+  }
+
+  allStates() {
+    return this.states
+  }
+
+  presentState() {
+    return this.present
+  }
+
+  isRenewed() {
+    return this.renewed
+  }
+
+  setFutureState(state) {
+    this.future = state
+  }
+
+  switchState() {
+    this.renewed = this.future !== undefined
+    this.present = this.future || this.present
+    this.future = undefined
+  }
+}
