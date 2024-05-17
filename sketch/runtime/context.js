8867478df755e301f0477473cc01d75a88941bb9
Elorfiniel <markgenthusiastic@gmail.com>
2024-05-18 02:15:01 +0800

    Update: differentiate assets and values, add game controls

--- sketch/runtime/context.js
+++ sketch/runtime/context.js
@@ -3,6 +3,10 @@ class GameContext {
     this.states = new GameStateManager()
     this.views = new SimpleDict()
     this.assets = new SimpleDict()
+    this.values = new SimpleDict()
     this.socket = new SocketManager()
+    this.display = new DisplayConvert()
+    this.space = undefined
+    this.game = undefined
   }
 }
