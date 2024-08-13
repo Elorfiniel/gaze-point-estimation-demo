56c217f2f70e02ec54df3669e5cafc51ee27211c
Elorfiniel <markgenthusiastic@gmail.com>
2024-08-13 16:59:20 +0800

    Update: extract common utils for other tools

--- estimator/tools/preview_record.py
+++ estimator/tools/preview_record.py
@@ -1,5 +1,6 @@
+from tool_utils import *
+
 import argparse
-import ast
 import logging
 import functools
 import numpy as np
@@ -367,41 +368,6 @@ def main_procedure(cmdargs: argparse.Namespace):
     generate_visualization(in_folder, out_file, **visual_kwargs)
 
 
-
-def active_root_logger():
-  stream_handler = logging.StreamHandler()
-  stream_handler.setLevel(logging.INFO)
-
-  formatter = logging.Formatter(
-    '[ %(asctime)s ] process %(process)d - %(levelname)s: %(message)s',
-    datefmt='%m-%d %H:%M:%S',
-  )
-  stream_handler.setFormatter(formatter)
-
-  root_logger = logging.getLogger('')
-  for handler in root_logger.handlers[:]:
-    root_logger.removeHandler(handler)
-  root_logger.addHandler(stream_handler)
-  root_logger.setLevel(logging.INFO)
-
-def update_kwargs_by_pop(kwargs, update_kwargs):
-  '''Update `kwargs` by poping the keys in `update_kwargs`. It's assumed that
-  `kwargs` specifies all the needed key-value pairs (as defaults) so that
-  `update_kwargs` can be shared across different functions.'''
-
-  for key in kwargs.keys():
-    if key in update_kwargs:
-      kwargs[key] = update_kwargs.pop(key)
-
-def parse_key_value(kv_string: str):
-  try:  # split the key-value pair, where key is a string, and value is a literal expression
-    key, value = kv_string.split(sep='=', maxsplit=1)
-    key, value = key.strip(), value.strip()
-    return key, ast.literal_eval(value)
-  except Exception as e:
-    raise argparse.ArgumentTypeError(f"invalid argument '{kv_string}', expecting 'key=value', "
-                                     "where key is a string, and value is a literal expression"
-                                     "such as strings, numbers, tuples, lists, dicts, ...")
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Visualize data captured with the gaze demo.')
---
+++ estimator/tools/tool_utils.py
@@ -0,0 +1,41 @@
+import argparse
+import ast
+import logging
+
+
+def active_root_logger():
+  '''Configure root logger for command line tools.'''
+
+  stream_handler = logging.StreamHandler()
+  stream_handler.setLevel(logging.INFO)
+
+  formatter = logging.Formatter(
+    '[ %(asctime)s ] process %(process)d - %(levelname)s: %(message)s',
+    datefmt='%m-%d %H:%M:%S',
+  )
+  stream_handler.setFormatter(formatter)
+
+  root_logger = logging.getLogger('')
+  for handler in root_logger.handlers[:]:
+    root_logger.removeHandler(handler)
+  root_logger.addHandler(stream_handler)
+  root_logger.setLevel(logging.INFO)
+
+def update_kwargs_by_pop(kwargs, update_kwargs):
+  '''Update `kwargs` by poping the keys in `update_kwargs`. It's assumed that
+  `kwargs` specifies all the needed key-value pairs (as defaults) so that
+  `update_kwargs` can be shared across different functions.'''
+
+  for key in kwargs.keys():
+    if key in update_kwargs:
+      kwargs[key] = update_kwargs.pop(key)
+
+def parse_key_value(kv_string: str):
+  try:  # split the key-value pair, where key is a string, and value is a literal expression
+    key, value = kv_string.split(sep='=', maxsplit=1)
+    key, value = key.strip(), value.strip()
+    return key, ast.literal_eval(value)
+  except Exception as e:
+    raise argparse.ArgumentTypeError(f"invalid argument '{kv_string}', expecting 'key=value', "
+                                     "where key is a string, and value is a literal expression"
+                                     "such as strings, numbers, tuples, lists, dicts, ...")
