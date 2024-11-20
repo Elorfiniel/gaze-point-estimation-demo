4aee6908c5ec233744b1fceacc07289730e9238f
Elorfiniel <markgenthusiastic@gmail.com>
2024-11-20 13:39:47 +0800

    Add: game loop demonstration

---
+++ sketch/debug.js
@@ -0,0 +1,20 @@
+const context = {}  // global context
+
+function setup() {
+  context.canvas = document.getElementById("canvas")
+  createCanvas(windowWidth, windowHeight, P2D, context.canvas)
+
+  context.space = new Space(80)
+  context.game = new GameSystem(windowWidth / 2, -2)
+}
+
+function draw() {
+  const spacebar = keyIsPressed && keyCode == 32
+
+  background(221, 230, 237)
+  context.space.draw()
+  context.game.draw()
+
+  context.space.update()
+  context.game.update(mouseX, mouseY, spacebar, true)
+}
