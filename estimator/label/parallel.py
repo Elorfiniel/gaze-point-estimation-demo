8867bfb98f4f738e0ee6580f9697e29a5bd47380
Elorfiniel <markgenthusiastic@gmail.com>
2024-12-19 16:51:08 +0800

    Update: optional arguments for running parallel task

--- estimator/label/parallel.py
+++ estimator/label/parallel.py
@@ -1,10 +1,8 @@
-from typing import Callable, Dict, Generator, Tuple
-
 import concurrent.futures as futures
 import logging
 
 
-def secure_done_fn(done_fn: Callable):
+def _secure_done_fn(done_fn):
   def wrapper(future: futures.Future):
     try:
       done_fn(future) # Anticipating potential exceptions
@@ -13,23 +11,33 @@ def secure_done_fn(done_fn: Callable):
 
   return wrapper
 
+def _parse_fn_and_args(task):
+  if not isinstance(task, tuple): task = (task, )
+  task = task + (4 - len(task)) * (None, )
+  task_fn, args, kwargs, done_fn = task
 
-ParaTask = Tuple[Callable, Tuple, Dict, Callable]
+  args = args if args is not None else tuple()
+  kwargs = kwargs if kwargs is not None else dict()
 
-def run_parallel(executor: futures.ProcessPoolExecutor,
-                 tasks: Generator[ParaTask, None, None]):
-  '''Run tasks in parallel. Each task is a tuple of (task_fn, args, kwargs, done_fn).
-  If no done_fn is provided (`done_fn=None`), a default done_fn will be used.
+  if done_fn is None:
+    done_fn = lambda f: f.result()
+  done_fn = _secure_done_fn(done_fn)
+
+  return task_fn, args, kwargs, done_fn
+
+def run_parallel(executor: futures.ProcessPoolExecutor, tasks):
+  '''Run tasks in parallel. Each task is either a callable task_fn or
+  a tuple of (task_fn, args, kwargs, done_fn). Note that args, kwargs
+  and done_fn are all optional parameters.
 
   Args:
     `executor`: process pool executor used for parallel tasks
-    `tasks`: a generator of tasks, each task is a tuple of (task_fn, args, kwargs, done_fn)
+    `tasks`: a generator of tasks, each task can either be a callable or a tuple of
+    (task_fn, args, kwargs, done_fn), where args, kwargs and done_fn are optional
   '''
 
   with executor:
-    for task_fn, args, kwargs, done_fn in tasks:
+    for task in tasks:
+      task_fn, args, kwargs, done_fn = _parse_fn_and_args(task)
       future = executor.submit(task_fn, *args, **kwargs)
-      if done_fn is None:
-        done_fn = lambda f: f.result()
-      done_fn = secure_done_fn(done_fn)
       future.add_done_callback(done_fn)
