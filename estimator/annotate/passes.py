4e0b759d82838faa1ce7800f5d9e7ded619803c6
Elorfiniel <markgenthusiastic@gmail.com>
2025-06-16 21:31:24 +0800

    Add new annotation pipeline

--- estimator/annotate/passes.py
+++ estimator/annotate/passes.py
@@ -1,3 +1,11 @@
+from runtime.es_config import EsConfig, EsConfigFns
+from runtime.log import runtime_logger
+from runtime.parallel import FunctionalTask, submit_functional_task
+
+import concurrent.futures as futures
+import os.path as osp
+
+
 class BasePass:
   def before_pass(self):
     '''Hook: called before the pass starts processing any data.'''
@@ -25,3 +33,53 @@ class BasePass:
       self.process_data(data)
 
     self.after_pass()
+
+
+class RecordingPass(BasePass):
+
+  PASSES = dict()
+
+  @staticmethod
+  def process(recording_path: str, an_config: EsConfig):
+    RecordingPass(recording_path, an_config).run()
+
+  def __init__(self, recording_path: str, an_config: EsConfig):
+    self.recording_path = recording_path
+    self.an_config = an_config
+
+  def after_pass(self):
+    rt_logger = runtime_logger(name='annotator').getChild('update')
+    rt_logger.info(f'finished processing for recording "{self.recording_path}"')
+
+  def collect_data(self):
+    pass_config = EsConfigFns.named_dict(self.an_config, 'recording_pass')
+    return (self.PASSES[p] for p in pass_config['passes'])
+
+  def process_data(self, data: BasePass):
+    data(self.recording_path, self.an_config).run()
+
+
+class MainPass(BasePass):
+  def __init__(self, record_path: str, recordings: list, an_config: EsConfig):
+    self.record_path = record_path
+    self.recordings = recordings
+    self.an_config = an_config
+
+  def before_pass(self):
+    pass_config = EsConfigFns.named_dict(self.an_config, 'main_pass')
+    self.executor = futures.ProcessPoolExecutor(max_workers=pass_config['num_workers'])
+
+  def after_pass(self):
+    self.executor.shutdown(wait=True)
+
+  def collect_data(self):
+    def task_generator():
+      for recording in self.recordings:
+        args = (osp.join(self.record_path, recording), self.an_config)
+        yield FunctionalTask(RecordingPass.process, *args)
+
+    return task_generator()
+
+  def process_data(self, data: FunctionalTask):
+    rt_logger = runtime_logger(name='annotator').getChild('parallel')
+    submit_functional_task(data, self.executor, rt_logger)
---
+++ estimator/annotator.py
@@ -0,0 +1,62 @@
+from annotate.passes import MainPass
+
+from runtime.es_config import EsConfig, EsConfigFns
+from runtime.miscellaneous import deep_update
+from runtime.log import runtime_logger
+
+import argparse
+import os
+import os.path as osp
+
+
+rt_logger = runtime_logger(name='annotator')
+
+
+def collect_recordings(cmdargs: argparse.Namespace):
+  if cmdargs.record_path:
+    record_path = osp.abspath(cmdargs.record_path)
+    recordings = [
+      item for item in os.listdir(record_path)
+      if osp.isdir(osp.join(record_path, item))
+    ]
+  if cmdargs.recording:
+    record_path = osp.dirname(osp.abspath(cmdargs.recording))
+    recordings = [osp.basename(osp.abspath(cmdargs.recording))]
+
+  recordings.sort(reverse=False)
+  rt_logger.info(f'collected {len(recordings)} recordings from "{record_path}"')
+
+  return record_path, recordings
+
+def collect_an_config(cmdargs: argparse.Namespace):
+  config_path = osp.abspath(cmdargs.config)
+  es_config_dict = EsConfig.from_toml(config_path).to_dict()
+
+  annot_config_path = osp.abspath(cmdargs.annot_config)
+  annot_config_dict = EsConfig.from_toml(annot_config_path).to_dict()
+
+  an_config = EsConfig(deep_update(es_config_dict, annot_config_dict))
+  EsConfigFns.set_config_path(an_config, config_path)
+
+  return an_config
+
+
+def main_procedure(cmdargs: argparse.Namespace):
+  record_path, recordings = collect_recordings(cmdargs)
+  an_config = collect_an_config(cmdargs)
+  MainPass(record_path, recordings, an_config).run()
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Annotate the collected recordings automatically.')
+
+  targets = parser.add_mutually_exclusive_group(required=True)
+  targets.add_argument('--record-path', type=str, help='The path to the collected recordings.')
+  targets.add_argument('--recording', type=str, help='The path to a specific recording.')
+
+  parser.add_argument('--config', type=str, default='estimator.toml',
+                      help='Configuration for the PoG estimator.')
+  parser.add_argument('--annot-config', type=str, default='annotator.toml',
+                      help='Configuration for this PoG annotator.')
+
+  main_procedure(parser.parse_args())
