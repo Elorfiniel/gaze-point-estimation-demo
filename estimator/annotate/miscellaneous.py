2b22cdac53f7a0037d6de50a8be168f7b5c223a6
Elorfiniel <markgenthusiastic@gmail.com>
2025-06-18 22:37:53 +0800

    Add helper function to load/dump JSON

--- estimator/annotate/miscellaneous.py
+++ estimator/annotate/miscellaneous.py
@@ -1,7 +1,25 @@
 from .base_pass import BasePass
 
+import json
+import os.path as osp
+
 
 def require_context(bpass: BasePass, context: dict, item_names: list):
   for item_name in item_names:
     if context.get(item_name) is None:
       raise RuntimeError(f'{bpass.PASS_NAME} requires "{item_name}" in context')
+
+
+def load_json(json_path: str, **kwargs):
+  json_path = osp.abspath(json_path)
+
+  with open(json_path, 'r') as json_file:
+    json_data = json.load(json_file, **kwargs)
+  return json_data
+
+
+def dump_json(json_path: str, json_data: dict, **kwargs):
+  json_path = osp.abspath(json_path)
+
+  with open(json_path, 'w') as json_file:
+    json.dump(json_data, json_file, **kwargs)
